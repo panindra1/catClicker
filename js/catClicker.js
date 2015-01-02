123b4620a3bbcbd1f7888266a9ea3d80d203fc3a
Panindra <pani.1990@gmail.com>
2015-01-02 14:38:37 -0600

    dynamic creation of images

--- js/catClicker.js
+++ js/catClicker.js
@@ -1,5 +1,21 @@
-var clickCount = 0;
-function onCatClick() {
-	clickCount++;
-	alert(clickCount);
-}
+var catCount = 2;
+
+$(document).ready(function(){
+	var catClickCount = [];
+	for(var i = 0; i < catCount; i++) {
+		catClickCount[i] = 0;
+	}
+
+	var catNames = ["cuteCat", "prettyCat"];
+
+	for (var i = 0; i < catCount; i++) {
+		$('#dynamicImage').append("<div><label>" +catNames[i]+"</label></div><br />"); 
+		$('#dynamicImage').append("<img id = 'image"+i+"' src= 'images/image" +i+".jpg' width = 300px height = 300px> </img>" );
+		$('#dynamicImage').append("<div><label id = 'clickCount"+i+"'>image" +0+"></label></div><br />"); 
+		$('#image'+i).on( "click", { value: i }, function( event ) {
+			var val = parseInt(event.data.value)
+			catClickCount[val] = catClickCount[val] + 1;
+			$('#clickCount' + val).text(catClickCount[val]);
+		});
+	}
+	});
