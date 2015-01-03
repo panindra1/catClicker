921ffa093907bf8d6fc7a43589f1f8f549f62b3f
Panindra <pani.1990@gmail.com>
2015-01-03 08:33:15 -0600

    Added MVC pattern to the code.. Organized

--- js/catClicker.js
+++ js/catClicker.js
@@ -1,21 +1,41 @@
-var catCount = 2;
 
 $(document).ready(function(){
-	var catClickCount = [];
-	for(var i = 0; i < catCount; i++) {
-		catClickCount[i] = 0;
-	}
+	var model = {
 
-	var catNames = ["cuteCat", "prettyCat"];
+		catCount : 5,
+		catClickCount : [],
+		catNames : ["cuteCat", "prettyCat", "niceCat", "cutiePie", "NicePic"]
+	};
 
-	for (var i = 0; i < catCount; i++) {
-		$('#dynamicImage').append("<div><label>" +catNames[i]+"</label></div><br />"); 
-		$('#dynamicImage').append("<img id = 'image"+i+"' src= 'images/image" +i+".jpg' width = 300px height = 300px> </img>" );
-		$('#dynamicImage').append("<div><label id = 'clickCount"+i+"'>image" +0+"></label></div><br />"); 
-		$('#image'+i).on( "click", { value: i }, function( event ) {
-			var val = parseInt(event.data.value)
-			catClickCount[val] = catClickCount[val] + 1;
-			$('#clickCount' + val).text(catClickCount[val]);
-		});
-	}
-	});
+	var controller = {
+		init : function() {
+			//model.init();
+			view.init();
+		},
+		addCount: function(val) {
+			if(isNaN(model.catClickCount[val])) {
+					model.catClickCount[val] = 1;	
+			}
+			else {
+				model.catClickCount[val] = model.catClickCount[val] + 1;	
+			}
+			
+			$('#clickCount' + val).text(model.catClickCount[val]);
+		}
+	};
+
+	var view = {
+		init: function() {
+			for (var i = 0; i < model.catCount; i++) {
+			$('#dynamicImage').append("<div><label>" +model.catNames[i]+"</label></div><br />"); 
+			$('#dynamicImage').append("<img id = 'image"+i+"' src= 'images/image" +i+".jpg' width = 300px height = 300px> </img>" );
+			$('#dynamicImage').append("<div><label id = 'clickCount"+i+"'></label></div><br />"); 
+			$('#image'+i).on( "click", { value: i }, function( event ) {
+				controller.addCount(parseInt(event.data.value));
+			});		
+		  }
+		}
+	};
+	
+	controller.init();	
+});
